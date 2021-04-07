6ef7f43940567d636cce1f82060af52ddc467923
TranHoaiThu20 <tranhoaithu122001@gmail.com>
2021-04-07 20:52:45 +0700

    thay đổi các cảnh tiêu đề

--- application/views/site/Vleft.php
+++ application/views/site/Vleft.php
@@ -4,7 +4,7 @@
                     <div class="tit-thongbao">
                         <div class="clearfix vi-header">
                             <div class="vi-left-title pull-left">
-                                <a href=""><img src="{public_url('site')}/img/t-dangcai.jpg" alt=""></a>
+                                <a href="{base_url('dvdangcai')}"><img src="{public_url('site')}/img/t-dangcai.png" alt=""></a>
                                 <!-- <div class="thongbao">Thông báo</div> -->
                             </div>
                         </div>
@@ -14,6 +14,9 @@
                             <a href="{base_url('baiviet/')}{$dangcai[0]['sIDBaiviet']}"  style="display: block; font-size: 16px;
     color: #337ab7; text-align:justify;">{$dangcai[0]['tTomTat']}</a>
                         </div>
+                        <div class="xemthem">
+                            <a href="{base_url('dvdangcai')}">Xem thêm >></a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -22,7 +25,8 @@
                     <div class="tit-thongbao">
                         <div class="clearfix vi-header">
                             <div class="vi-left-title pull-left">
-                                <img src="{public_url('site')}/img/t-gioithieu.png" alt="">
+                                <!-- <img src="{public_url('site')}/img/t-gioithieu.png" alt=""> -->
+                                <a href="{base_url('gioithieu')}"><img src="{public_url('site')}/img/t-gioithieu.png" alt=""></a>
                                 <!-- <div class="thongbao">Thông báo</div> -->
                             </div>
                         </div>
@@ -31,7 +35,8 @@
                     {foreach $gioithieu as $r}
                         <div class="tintuc row">
                             <div class="icon col-lg-1">
-                            <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                            <img src="{public_url('site')}/img/logo-sach.png" alt="">
+                            <!-- <i class="fa fa-file-text-o" aria-hidden="true"></i> -->
                             </div>
                             <div class="tieude col-lg-10">
                                 <a href="{base_url('baiviet/')}{$r.sIDBaiviet}">{$r.sTieuDe}</a>
@@ -49,7 +54,8 @@
                     <div class="tit-thongbao">
                         <div class="clearfix vi-header">
                             <div class="vi-left-title pull-left">
-                                <img src="{public_url('site')}/img/tittle11.png" alt="">
+                                <!-- <img src="{public_url('site')}/img/tittle11.png" alt=""> -->
+                                <a href="{base_url('thongbao')}"><img src="{public_url('site')}/img/tittle11.png" alt=""></a>
                                 <!-- <div class="thongbao">Thông báo</div> -->
                             </div>
                         </div>
@@ -76,8 +82,34 @@
                     <div class="tit-thongbao">
                         <div class="clearfix vi-header">
                             <div class="vi-left-title pull-left">
-                                <img src="{public_url('site')}/img/t-ontap.png" alt="">
-                                <!-- <div class="thongbao">Thông báo</div> -->
+                                <a href="{base_url('ontap')}"><img src="{public_url('site')}/img/t-ontap.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body nd-thongbao">
+                    {foreach $ontap as $r}
+                        <div class="tintuc row">
+                            <div class="icon col-lg-1">
+                            <i class="fa fa-book" aria-hidden="true"></i>
+                            </div>
+                            <div class="tieude col-lg-10">
+                                <a href="{base_url('baiviet/')}{$r.sIDBaiviet}">{$r.sTieuDe}</a>
+                                <div class="date">
+                                    <i class="fa fa-calendar" aria-hidden="true"> {date("d-m-Y", $r.dNgayDang)}</i>
+                                </div>
+                            </div>
+                        </div>
+                    {/foreach}
+                    </div>
+                </div>
+            </div>
+
+            <div class="b_left col-lg-12">
+                <div class="l_content col-lg-12">
+                    <div class="tit-thongbao">
+                        <div class="clearfix vi-header">
+                            <div class="vi-left-title pull-left">
+                                <a href="{base_url('ontap')}"><img src="{public_url('site')}/img/t-ontap.png" alt=""></a>
                             </div>
                         </div>
                     </div>
